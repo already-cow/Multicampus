c39bcebde97bd4ea6185e7f1d8a1f0e607fc1d77
alreadycow <alreadycow_@naver.com>
2023-08-07 13:15:40 +0900

    java 수정파일

---
+++ 02_java/java_study/src/java_study/New/chapter06/MyRoom.java
@@ -0,0 +1,21 @@
+package chapter06;
+
+public class MyRoom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Phone p1 = new Phone();
+		p1.internet();
+		p1.color = "검정";
+		p1.shape = "폴더";
+
+		Phone p2 = new Phone();
+		p2.talkByKakao();
+		p2.color = "빨강";
+		p2.shape = "네모";
+
+		System.out.println(p2.color);
+		System.out.println(p2.shape);
+	}
+
+}
